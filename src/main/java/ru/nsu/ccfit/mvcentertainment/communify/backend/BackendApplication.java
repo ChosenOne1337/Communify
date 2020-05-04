689c7a22bf6d6f6d13245ba68984df831582b3d1
Max <m.golikov@g.nsu.ru>
2020-05-04 22:31:38 +0700

    Added maven

--- src/main/java/ru/nsu/ccfit/mvcentertainment/communify/backend/BackendApplication.java
+++ src/main/java/ru/nsu/ccfit/mvcentertainment/communify/backend/BackendApplication.java
@@ -3,6 +3,9 @@ package ru.nsu.ccfit.mvcentertainment.communify.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class BackendApplication {
 
