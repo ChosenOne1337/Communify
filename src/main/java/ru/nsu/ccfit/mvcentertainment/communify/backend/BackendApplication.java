76cbbe4520f8a0f390440e8d16001183b32cc61c
Vladislav <vladcheremnov@gmail.com>
2020-05-04 23:49:33 +0700

    Revert "Added maven"
    This reverts commit 689c7a22bf6d6f6d13245ba68984df831582b3d1

--- src/main/java/ru/nsu/ccfit/mvcentertainment/communify/backend/BackendApplication.java
+++ src/main/java/ru/nsu/ccfit/mvcentertainment/communify/backend/BackendApplication.java
@@ -3,9 +3,6 @@ package ru.nsu.ccfit.mvcentertainment.communify.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @SpringBootApplication
 public class BackendApplication {
 
